7e319686cb1b21b80a496d72f178e19d6dc97492
Noob-developer-sobur <soburkhan210@gmail.com>
2023-07-09 13:03:34 +0600

    js pactice

---
+++ js/min.js
@@ -0,0 +1,96 @@
+var j = 2
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+
+var j = 3
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 4
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 5
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 6
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 7
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 8
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 9
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 10
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 11
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 12
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 13
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 14
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 15
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 16
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 17
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 18
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 19
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
+var j = 20
+for( i = 1; i <= 10; i++){
+    var result = j*i
+    document.write( j + "X" + i + "=" + result + "<br><br>") 
+}
